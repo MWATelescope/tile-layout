c72aca36f0909b4613d6139fe4d33d938bcd3674
Andrew Williams <Andrew.Williams@curtin.edu.au>
2014-02-19 10:12:43 +0800

    bugfixes

--- mwatiles/tilconv.py
+++ mwatiles/tilconv.py
@@ -1,7 +1,12 @@
 import math
 
-f = open('/tmp/tilepos.csv','r')
-tlist = [map(float, t.split(',')) for t in f.readlines()]
+f = open('newpos.csv','r')
+pointlist = []
+for line in f.readlines()[1:]:
+  print line
+  pointlist.append((line.split(',')[0], map(float, line.split(',')[1:])))
+
+#tlist = [map(float, t.split(',')) for t in f.readlines()[1:]]
 
 
 def sexstring(value=0, sp=':', fixed=False, dp=None):
@@ -105,15 +110,21 @@ SLONG = (LONG133-LONG107)/(E133-E107)
 
 
 class Pos(object):
-  def __init__(self, e, n, h):
-    self.lat = 0.0
-    self.long = 0.0
-    self.e = e
-    self.n = n
+  def __init__(self, e=None, n=None, h=None, lat=None, long=None):
     self.h = h
-    self.calc()
+    if (e is not None) and (n is not None):
+      self.lat = 0.0
+      self.long = 0.0
+      self.e = e
+      self.n = n
+      self.calclatlong()
+    elif (lat is not None) and (long is not None):
+      self.lat = lat
+      self.long = long
+      self.e = 0.0
+      self.n = 0.0
 
-  def calc(self):
+  def calclatlong(self):
     de = self.e - E107
     dn = self.n - N151
     self.lat = dn*SLAT + LAT151
@@ -123,40 +134,57 @@ class Pos(object):
     return "Pos: E=%15.3f, N=%15.3f, Lat=%s, Long=%s" % (self.e, self.n, sexstring(self.lat,dp=6), sexstring(self.long,dp=6))
 
 
-tiles = {}
-recs = {}
-
-for i in range(len(tlist)):
-  t = tlist[i]
-  tiles[int(t[0])] = Pos(t[1],t[2],t[3])
-  if divmod(i,8)[1] == 0:
-    recs[divmod(i,8)[0]+1] = Pos(t[4],t[5],t[3])
-
 TEMPLATE = """
    <waypoint>
       <name id="%(name)s">%(name)s</name>
-      <coord lat="%(lat)f" lon="%(lon)f"/>
+      <coord lat="%(lat)f" lon="%(long)f"/>
       <type>Waypoint</type>
    </waypoint>
 """
 
-outf = open('tiles.loc', 'w')
+#tiles = {}
+#recs = {}
+
+waypoints = {}
+
+#for i in range(len(tlist)):
+#  t = tlist[i]
+#  tiles[int(t[0])] = Pos(t[1],t[2],t[3])
+#  if t[0].start
+#  if divmod(i,8)[1] == 0:
+#    recs[divmod(i,8)[0]+1] = Pos(t[4],t[5],t[3])
+
+for point in pointlist:
+  name, coords = point
+  p = Pos(h=coords[3], long=coords[4], lat=coords[5])
+  waypoints[name] = p
+
+outf = open('mwawaypoints.loc', 'w')
 
 outf.write('<?xml version="1.0" encoding="UTF-8"?>\n <loc version="1.0" src="Groundspeak">\n')
 
-for id,t in tiles.items():
-  r = recs[divmod(id,10)[0]]
-  outf.write(TEMPLATE % {'name':'T'+str(id), 'lat':t.lat, 'lon':t.long})
+nlist = waypoints.keys()
+nlist.sort()
+
+for n in nlist:
+  p = waypoints[n]
+  outf.write(TEMPLATE % {'name':n, 'lat':p.lat, 'long':p.long})
+
+#for id,t in tiles.items():
+#  r = recs[divmod(id,10)[0]]
+#  outf.write(TEMPLATE % {'name':'T'+str(id), 'lat':t.lat, 'lon':t.long})
 
 outf.write('</loc>\n')
+
 outf.close()
 
-outf = open('recs.loc', 'w')
-outf.write('<?xml version="1.0" encoding="UTF-8"?>\n <loc version="1.0" src="Groundspeak">\n')
+#outf = open('recs.loc', 'w')
+#outf.write('<?xml version="1.0" encoding="UTF-8"?>\n <loc version="1.0" src="Groundspeak">\n')
 
-for id,r in recs.items():
-  outf.write(TEMPLATE % {'name':'REC'+str(id), 'lat':r.lat, 'lon':r.long})
+#for id,r in recs.items():
+#  outf.write(TEMPLATE % {'name':'REC'+str(id), 'lat':r.lat, 'lon':r.long})
 
-outf.write('</loc>\n')
-outf.close()
+#outf.write('</loc>\n')
+
+#outf.close()
 
