7fca5932ceb9e3894fcfeb1166d5350ff58df9e8
Andrew Williams <Andrew.Williams@curtin.edu.au>
2016-07-04 16:55:16 +0800

    Added cables, fixed rotation

--- aavs/aavsmodels.py
+++ aavs/aavsmodels.py
@@ -1,11 +1,19 @@
 
-import os
+from PIL import ImageGrab
 
 import visual
 from visual import color
 from visual import materials
 from visual import Polygon as P
 from visual import vector as V
+
+scene = visual.display(x=0, y=0, width=1024, height=768)
+scene.autoscale = False
+scene.forward = (0, 1, -1)
+scene.up = (0, 0, 1)
+scene.range = 25
+
+MOVIE = False   # Do we capture frames to disk?
 
 # Base object
 square = P([(-0.5, -0.475), (-0.5, 0.475), (0.5, 0.475), (0.5, -0.475)])
@@ -24,6 +32,8 @@ WIREMAT = None
 PIPEMAT = None
 POLEMAT = materials.plastic
 GMAT = materials.texture(data=materials.loadTGA('mro.tga'))
+
+GCOLOR = V(0.95, 0.59, 0.42)
 
 
 # A single pole, with loops:
@@ -131,6 +141,21 @@ def getxmas():
   return xmas, elements
 
 
+def getbundle(spos=None):   # spos is  position that cable centre exits the box
+  blist = []
+  cframe = visual.frame()
+  startz = spos.z   # Height above ground that cable centre exits the APIU box
+  carc1 = visual.paths.arc(radius=startz / 2, angle1=visual.pi, angle2=visual.pi/2, up=(0,-1,0))
+  carc1.pos = [p + spos + V(0, 0, -startz / 2) for p in carc1.pos]
+  c1 = visual.curve(frame=cframe, pos=carc1, radius=0.08 / 2, color=color.blue)
+  carc2 = visual.paths.arc(radius=startz / 2, angle1=visual.pi, angle2=visual.pi/2, up=(0,1,0))
+  carc2.pos = [p + spos + V(startz, 0, -startz / 2) for p in carc2.pos]
+  c2 = visual.curve(frame=cframe, pos=carc2, radius=0.08 / 2, color=color.blue)
+  c2.append(pos=spos + V(1.5, 0, -startz), color=GCOLOR)
+  blist.append(c1)
+  return cframe, blist
+
+
 def getapiu():
   box = visual.box(pos=V(0, 0, 0.800/2 + 0.12), length=0.800, height=1.050, width=0.800, color=color.white)
   hat = visual.pyramid(pos=V(0, 0, 0.800 + 0.12), size=V(0.06, 1.050, 0.800), axis=V(0,0,1), color=color.white)
@@ -144,6 +169,101 @@ def getapiu():
   ssn = visual.box(pos=V(0, 1.050 / 2 + 0.030, 0.800 / 2 + 0.12), length=0.800, height=0.003, width=0.800, color=color.white)
   sse = visual.box(pos=V(-0.800 / 2 - 0.030, 0, 0.600 / 2 + 0.12), length=0.003, height=1.050, width=0.600, color=color.white)
   ssw = visual.box(pos=V(0.800 / 2 + 0.030, 0, 0.600 / 2 + 0.12), length=0.003, height=1.050, width=0.600, color=color.white)
+  cbylist = [-0.2775, -0.1255, 0.1255, 0.2775]
+  cblist = []
+  for cy in cbylist:
+    cblist.append(visual.cylinder(pos=V(-0.800/2, cy, 0.700 + 0.120), axis=(-0.100, 0, 0), radius=0.01))
+    cblist.append(visual.helix(pos=V(-0.800 / 2 - 0.150, cy, 0.700 + 0.120 - 0.05),
+                               axis=(0.05, 0, 0),
+                               radius=0.050,
+                               thickness=0.01,
+                               coils=1,
+                               up=(0,1,0),
+                               color=color.white))
+    cb, tlist = getbundle(spos=V(-0.800 / 2, cy, 0.700 + 0.120 - 0.05))
+    cblist += tlist
+    cb.rotate(angle=visual.pi, axis=(0,0,1), origin=V(-0.800 / 2, cy, 0.700 + 0.120 - 0.05))
+  for cy in cbylist:
+    cblist.append(visual.cylinder(pos=V(0.800 / 2, cy, 0.700 + 0.120), axis=(0.100, 0, 0), radius=0.01))
+    cblist.append(visual.helix(pos=V(0.800 / 2 + 0.10, cy, 0.700 + 0.120 - 0.05),
+                               axis=(0.05, 0, 0),
+                               radius=0.050,
+                               thickness=0.01,
+                               coils=1,
+                               up=(0, 1, 0),
+                               color=color.white))
+    cb, tlist = getbundle(spos=V(0.800 / 2, cy, 0.700 + 0.120 - 0.05))
+    cblist += tlist
+
+
+def gettreecable(treepos=V(0,0,0)):
+  cframe = visual.frame()
+  cstartpos = treepos + (0, 0, 0.4)    # Where the cable leaves the xmas tree trunk
+  theta = visual.atan2(treepos.y, treepos.x)
+
+  carc = visual.paths.arc(radius=0.37, angle1=-visual.pi/2, angle2=0, up=(0,-1,0))
+  carc.pos = [p + treepos + V(0.37, 0, 0.4) for p in carc.pos[::-1]]
+
+  c1 = visual.curve(frame=cframe, pos=carc, radius=0.0129 / 2, color=color.blue)
+  c1.append(pos=treepos + V(1.5, 0, -0.065), color=GCOLOR)
+  cframe.rotate(angle=theta + visual.pi, axis=(0, 0, 1), origin=cstartpos)
+
+
+recording = False
+playing = False
+framenumber = 0
+capnumber = 1
+viewlist = []
+
+
+def processClick(event):
+  global framenumber, capnumber, recording, playing, scene, viewlist
+  try:      # Key pressed:
+    s = event.key
+  except AttributeError:
+    s = None
+
+  if s is not None:
+    if s == 'c':
+      visual.sleep(0.05)
+      im = ImageGrab.grab((10,40, 1260,924))
+      im.save('frames/aavs%05d.png' % capnumber)
+      capnumber += 1
+    elif s == 'r':
+      viewlist = []
+      recording = True
+      framenumber = 0
+      playing = False
+      print "Recording views"
+    elif s == 's':
+      recording = False
+      playing = False
+      framenumber = 0
+      print "Stopped, we have %d frames of viewpoints recorded" % len(viewlist)
+      print viewlist
+    elif s == 'p':
+      recording = False
+      if playing:
+        playing = False
+        print "Playing paused at frame number %d of %d" % (framenumber, len(viewlist))
+      else:
+        playing = True
+        print "Playing started at frame number %d of %d" % (framenumber, len(viewlist))
+    elif s == 'left':
+      f = visual.rotate(vector=scene.forward, angle=visual.pi / 180, axis=V(0, 0, 1))
+      scene.forward = f
+    elif s == 'right':
+      f = visual.rotate(vector=scene.forward, angle=-visual.pi / 180, axis=V(0, 0, 1))
+      scene.forward = f
+    else:
+      print "Key pressed: '%s'" % s
+  else:             # Mouse clicked:
+    clickedpos = event.pickpos   # The 3D position on the surface of the object clicked on
+    if clickedpos:
+      scene.center = clickedpos  # Change the camera centre position
+
+
+scene.bind('click keydown', processClick)
 
 apiu = getapiu()
 
@@ -160,11 +280,29 @@ for line in lines:
   bases.append(b)
   tree, elist = getxmas()
   tree.pos = (x, y, 0.065)
+  cable = gettreecable(tree.pos)
 
 eaxis = visual.arrow(pos=V(0, 0, 0), axis=V(2, 0, 0), color=color.blue, shaftwidth=0.1, fixedwidth=True, opacity=0.2)
 naxis = visual.arrow(pos=V(0, 0, 0), axis=V(0, 2, 0), color=color.blue, shaftwidth=0.1, fixedwidth=True, opacity=0.2)
 
+
 framenumber = 0
 while True:
-  os.system('screencapture.exe frames/aavs%05d.jpg VPython' % framenumber)
-  visual.rate(1)
+  if recording:
+    viewlist.append(scene.forward.astuple())
+
+  if playing:
+    if framenumber < len(viewlist):
+      f = viewlist[framenumber]
+      scene.forward = f
+      print framenumber, f, scene.forward
+
+    if MOVIE:
+      visual.sleep(0.1)
+      im = ImageGrab.grab((10, 40, 1260, 924))
+      im.save('frames/aavs%05d.png' % framenumber)
+
+    framenumber += 1
+
+  if not (MOVIE and playing):
+    visual.rate(10)
